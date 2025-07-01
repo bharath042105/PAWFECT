19c4a8725da65da9a1c46880a4fbe1602578e18d
bharath042105 <150548268+bharath042105@users.noreply.github.com>
2025-07-01 14:32:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -393,5 +393,7 @@ if __name__ == '__main__':
                 username TEXT NOT NULL
             )
         ''')
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+        port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
+    
 
